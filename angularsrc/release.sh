1761dba36ebd0d450593e21a4ea079883e51f43a
sam <samson.bassett@gmail.com>
2019-03-23 15:16:59 -0700

    commit release script and better flex

--- angularsrc/release.sh
+++ angularsrc/release.sh
@@ -1,14 +1,19 @@
-  
 
+commit="'$*'"
+
+echo "ng build"
 ng build --prod --base-href lothrazar.github.io
 
+echo "deploy dist"
 cd dist/lothrazarpages
 
 mv * ../../../
 
+echo "uploading"
 git add -A
-git commit -am 'release'
+git commit -am "$commit"
 git push o master
 
 
+echo "release complete: $commit"
 
