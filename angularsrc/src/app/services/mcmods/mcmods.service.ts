9f4464d21470c81f927a5ae55cfdb456ff79eca6
sam <samson.bassett@gmail.com>
2019-03-23 22:01:38 -0700

    'mod order'

--- angularsrc/src/app/services/mcmods/mcmods.service.ts
+++ angularsrc/src/app/services/mcmods/mcmods.service.ts
@@ -8,11 +8,38 @@ export class McmodsService {
   constructor() { }
 
   getMods(): IModPage[] {
-    const mods: IModPage[] = [{
+    const mods: IModPage[] = [
+    {
+      id: 239286,
+      name: 'Cyclic',
+      crafttweaker: true,
+      curse: 'https://minecraft.curseforge.com/projects/cyclic',
+      tracker: 'https://github.com/Lothrazar/Cyclic/issues'
+    },
+    {
+      id: 268495,
+      name: 'Simple Storage Network',
+      curse: 'https://minecraft.curseforge.com/projects/simple-storage-network',
+      tracker: 'https://github.com/Lothrazar/Storage-Network/issues'
+    },
+    {
       id: 233659,
       name: 'Similsax Transtructors',
       curse: 'https://minecraft.curseforge.com/projects/similsax-transtructors',
       tracker: 'https://github.com/PrinceOfAmber/Similsax-Transtructors/issues'
+    },
+    {
+      id: 231837,
+      name: 'Ocean Floor Control',
+      curse: 'https://minecraft.curseforge.com/projects/ocean-floor-clay-sand-and-dirt',
+      tracker: 'https://github.com/PrinceOfAmber/OceanFloorControl/issues'
+    },
+    {
+      id: 295780,
+      crafttweaker: true,
+      name: 'Roots Classic',
+      curse: 'https://minecraft.curseforge.com/projects/roots-classic',
+      tracker: 'https://github.com/PrinceOfAmber/RootsClassic/issues'
     },
     {
       id: 305561,
@@ -51,36 +78,10 @@ export class McmodsService {
       curse: 'https://minecraft.curseforge.com/projects/giant-player-boss' 
     },
     {
-      id: 231837,
-      name: 'Ocean Floor Control',
-      curse: 'https://minecraft.curseforge.com/projects/ocean-floor-clay-sand-and-dirt',
-      tracker: 'https://github.com/PrinceOfAmber/OceanFloorControl/issues'
-    },
-    {
-      id: 295780,
-      crafttweaker: true,
-      name: 'Roots Classic',
-      curse: 'https://minecraft.curseforge.com/projects/roots-classic',
-      tracker: 'https://github.com/PrinceOfAmber/RootsClassic/issues'
-    },
-    {
-      id: 268495,
-      name: 'Simple Storage Network',
-      curse: 'https://minecraft.curseforge.com/projects/simple-storage-network',
-      tracker: 'https://github.com/Lothrazar/Storage-Network/issues'
-    },
-    {
       id: 313919,
       hidden: true,
       name: 'SSN Addon',
       curse: 'https://minecraft.curseforge.com/projects/simple-cables-storage-network-addon'
-    },
-    {
-      id: 239286,
-      name: 'Cyclic',
-      crafttweaker: true,
-      curse: 'https://minecraft.curseforge.com/projects/cyclic',
-      tracker: 'https://github.com/Lothrazar/Cyclic/issues'
     },
     { 
       id: 273711,
