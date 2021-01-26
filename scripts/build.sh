67e0c06fd03b331e8c9aff74a9155ea01ea6789f
lothrazar <samson.bassett@gmail.com>
2021-01-25 21:22:47 -0800

    import htaccess changes from server. add older versions. build script custom target

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# run from root
-
-rm -f dist
-
-#echo "build"
 ng build --prod --base-href lothrazar.net
 
+# push and overwrite
+cp -r ./dist/lothrazarpages/* ./release
+
+# was not picked up by build
+cp .htaccess ./release/.htaccess
 
 echo " ****** "
-echo "Build complete; view dist/index.html"
+echo "Build complete; view releases/index.html"
 echo " ****** "
 
