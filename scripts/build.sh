8f90173451eaebc5845894d342ba531f79e9a18e
lothrazar <samson.bassett@gmail.com>
2021-01-25 22:34:30 -0800

    build script upgrade to clean up

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,14 +1,35 @@
 #!/bin/bash
 
+# since ng always messes with dist
+# this will build and deploy to a git-folder for releases 
+
+# folder setup if not exist
+mkdir -p dist
+mkdir -p release
+
+# clean dist folder
+rm -Rf dist
+
+# clean release folder, but do NOT delete the .git folder
+cd release
+rm -rf * 
+cd ..
+
+# run the build
 ng build --prod --base-href lothrazar.net
 
-# push and overwrite
+# push it over to release folder
 cp -r ./dist/lothrazarpages/* ./release
 
-# was not picked up by build
+# was not picked up by build. so overwrite this
 cp .htaccess ./release/.htaccess
 
 echo " ****** "
-echo "Build complete; view releases/index.html"
+echo "Build ready in /release/"
 echo " ****** "
 
+cd release
+
+git add -A
+
+git status
--- src/app/components/tile-minecraftmod/mcmod.component.ts
+++ src/app/components/tile-minecraftmod/mcmod.component.ts
@@ -11,7 +11,6 @@ export class McmodComponent implements OnInit {
   @Input() mod: IModPage;
 
   constructor() {
-
   }
 
   ngOnInit() {
